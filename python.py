022bacbc0c2ee5518f5a14cfe1471bb64da95800
neeratipooja <116306537+neeratipooja@users.noreply.github.com>
2022-10-26 21:08:58 +0530

    Add files via upload

---
+++ python.py
@@ -0,0 +1,17 @@
+'''
+DATE=26th oct 2022
+DAY= wednesday
+TOPIC= strings 
+AUTHOR= KRISHNA POOJA
+'''
+print(type('pooja'))
+print(type('3'))
+print(type('0.235'))
+a=4
+print(str(a )+'+b')
+print(str(a))
+print('')      #empty
+print("")
+print('pooja\n' )
+print('krishna')
+print('pooja','krishna')
